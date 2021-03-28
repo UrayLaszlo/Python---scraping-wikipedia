388e8443b87bfa55db75c22ff5e437765ab3cf13
Laszlo <uray077@gmail.com>
2021-03-28 20:21:42 +0100

    scraping table file

--- table_scraper.py
+++ table_scraper.py
@@ -12,10 +12,8 @@ content = BeautifulSoup(page.text, "html.parser")
 page_title = content.title.text[:content.title.text.find("-")]
 table = content.find("table", class_="wikitable sortable")
 
-d = pd.read_html(str(table))
+df = pd.read_html(str(table).replace('‡',' '))
 
-df = d[0]
-
-print(len(d))
-print(df.info())
-#print(table.text.strip())
+print(df)
+#print(df.info())
+print(table.text)
