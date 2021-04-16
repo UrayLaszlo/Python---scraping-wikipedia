7f7072371a46be164da6dc2a276366fdf355bdf8
Laszlo <uray077@gmail.com>
2021-04-16 21:09:25 +0100

    loop through table

--- table_scraper.py
+++ table_scraper.py
@@ -6,25 +6,30 @@ import pandas as pd
 # Make HTTP get request
 url = "https://en.wikipedia.org/wiki/List_of_NFL_franchise_owners"
 page = requests.get(url)
+#print(page.status_code)
 
 # Parse content
 content = BeautifulSoup(page.text, "html.parser")
 
+# Get page title
 page_title = content.title.text[:content.title.text.find("-")]
 #print(page_title)
 
+# Get data table
 table = content.find("table", class_="wikitable sortable")
 #print(table.text)
+# Save in Pandas dataframe?
+#df = pd.read_html(str(table))
+#print(df)
 
-headers = [h.text for h in table.find_all('th')]
+for team in table.find_all('tbody'):
+    rows = team.find_all('tr')
+    for row in rows:
+        nfl_team = row.find('td', class_="reference").text
+        print(nfl_team)
+
+#print(table)
+
+#headers = [h.text for h in table.find_all('th')]
 #print(headers)
-
-df = pd.read_html(str(table))
-print(df)
-
-
-#rows = table.find_all('tr').text
-
-#print(df.info())
-#print(table.text)
 
